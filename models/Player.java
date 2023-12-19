be355946acb407ff6ac7aec0ba37c4866c1e382b
Rony Lee <rongyil33@gmail.com>
2023-12-19 12:28:25 +0800

    Add Player class

--- models/Player.java
+++ models/Player.java
@@ -1,3 +1,17 @@
+package models;
+
 public class Player {
-    
+    private int score;
+
+    Player(int score) {
+        this.score = score;
+    }
+
+    int getPlayerScore() {
+        return score;
+    }
+
+    void setPlayerScore(int score) {
+        this.score = score;
+    }
 }
