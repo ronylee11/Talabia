418bf31d4da764165aff507b30d35252921195d1
Rony Lee <rongyil33@gmail.com>
2024-01-11 13:11:56 +0800

    Add piece in pieceview

--- views/PieceView.java
+++ views/PieceView.java
@@ -8,6 +8,8 @@ import models.Piece;
 @SuppressWarnings("serial")
 public class PieceView extends JButton {
     static int pieceSpawned = 0; // max: 28
+    static String[] pointLocations = {"a2", "b2", "c2", "d2", "e2", "f2", "g2", "a5", "b5", "c5", "d5", "e5", "f5", "g5"};
+    Piece piece;
 
     public PieceView() {
         super(new ImageIcon("assets/arrow-y.jpg"));
