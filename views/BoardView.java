60c05c7eb73e5e1829754a867cf19d63faa8bac4
Rony Lee <rongyil33@gmail.com>
2024-01-12 10:44:49 +0800

    Add board visualization comment

--- views/BoardView.java
+++ views/BoardView.java
@@ -11,6 +11,14 @@ import controllers.BoardController;
 public class BoardView extends JFrame implements ActionListener {
     private Board b = new Board();
     PieceView[] buttons = new PieceView[42];
+    // buttons = [
+    // 'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6',
+    // 'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5',
+    // 'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4',
+    // 'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3',
+    // 'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2',
+    // 'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1'
+    // ]
 
     public BoardView() {
         super("Talabia Chess");
