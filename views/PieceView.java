31c4af1532b53a5beb97bd16f55cc937a237e344
sally <1211104401@student.mmu.edu.my>
2024-01-15 16:57:22 +0800

    change pieces model

--- views/PieceView.java
+++ views/PieceView.java
@@ -1,12 +1,17 @@
 package views;
 
-
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.Dimension;
 import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+import models.Hourglass;
 import models.Piece;
+import models.Plus;
 import models.Point;
+import models.Sun;
+import models.Time;
 
 @SuppressWarnings("serial")
 public class PieceView extends JButton {
@@ -17,9 +22,13 @@ public class PieceView extends JButton {
                                                 "a6", "b6", "c6", "d6", "e6", "f6", "g6"};
     private String[] notationSequence = generateNotationSequence();
     private String boardCoordinate = "";
+    
     Piece piece;
     Point[] point;
-
+    Plus[] plus;
+    Hourglass[] hourglass;
+    Time[] time;
+    Sun[] sun;
 
     String[] generateNotationSequence() {
         ArrayList<String> notationSequence = new ArrayList<String>();
@@ -33,7 +42,7 @@ public class PieceView extends JButton {
 
     public PieceView() {
         // add image
-        //super(new ImageIcon("assets/arrow-y.jpg"));
+        //super(new ImageIcon("assets/arrow-y.png"));
         super();
         // add coordinate
         pieceSpawned++;
@@ -52,44 +61,44 @@ public class PieceView extends JButton {
                 } else if ((boardCoordinate.charAt(1) == '1')){ // yellow pieces
                     // plus yellow
                     if (notationSequence[pieceSpawned - 1].equals ("a1") || notationSequence[pieceSpawned - 1].equals("g1")){
-                        Point point = new Point("assets/plus-y", boardCoordinate);
+                        Plus plus = new Plus("assets/plus-y", boardCoordinate);
                         this.setIcon(new ImageIcon("assets/plus-y.png"));
                         break;
                     // hourglass yellow
                     } else if (notationSequence[pieceSpawned - 1].equals ("b1") || notationSequence[pieceSpawned - 1].equals("f1")){
-                        Point point = new Point("assets/hourglass-y", boardCoordinate);
+                        Hourglass houglass = new Hourglass("assets/hourglass-y", boardCoordinate);
                         this.setIcon(new ImageIcon("assets/hourglass-y.png"));
                         break;
                     // cross yellow
                     } else if (notationSequence[pieceSpawned - 1].equals ("c1") || notationSequence[pieceSpawned - 1].equals("e1")){
-                        Point point = new Point("assets/cross-y", boardCoordinate);
+                        Time time = new Time("assets/cross-y", boardCoordinate);
                         this.setIcon(new ImageIcon("assets/cross-y.png"));
                         break;
                     // sun yellow
                     } else {
-                        Point point = new Point("assets/sun-y", boardCoordinate);
+                        Sun sun = new Sun("assets/sun-y", boardCoordinate);
                         this.setIcon(new ImageIcon("assets/sun-y.png"));
                         break;
                     }
                 } else if ((boardCoordinate.charAt(1) == '6')){ // blue pieces
                     // plus blue
                     if (notationSequence[pieceSpawned - 1].equals ("a6") || notationSequence[pieceSpawned - 1].equals("g6")){
-                        Point point = new Point("assets/plus-b", boardCoordinate);
+                        Plus plus = new Plus("assets/plus-b", boardCoordinate);
                         this.setIcon(new ImageIcon("assets/plus-b.png"));
                         break;
                     // hourglass blue
                     } else if (notationSequence[pieceSpawned - 1].equals ("b6") || notationSequence[pieceSpawned - 1].equals("f6")){
-                        Point point = new Point("assets/hourglass-b", boardCoordinate);
+                        Hourglass hourglass = new Hourglass("assets/hourglass-b", boardCoordinate);
                         this.setIcon(new ImageIcon("assets/hourglass-b.png"));
                         break;
                     // cross blue
                     } else if (notationSequence[pieceSpawned - 1].equals ("c6") || notationSequence[pieceSpawned - 1].equals("e6")){
-                        Point point = new Point("assets/cross-b", boardCoordinate);
+                        Time time = new Time("assets/cross-b", boardCoordinate);
                         this.setIcon(new ImageIcon("assets/cross-b.png"));
                         break;
                     // sun blue
                     } else {
-                        Point point = new Point("assets/sun-b", boardCoordinate);
+                        Sun sun = new Sun("assets/sun-b", boardCoordinate);
                         this.setIcon(new ImageIcon("assets/sun-b.png"));
                         break;
                     }
