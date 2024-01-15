5b95794064d0ad24dd0ddaf3b0ec4c26a1628cdb
Rony Lee <rongyil33@gmail.com>
2024-01-15 21:44:14 +0800

    Reorganize Board to mvc

--- controllers/BoardController.java
+++ controllers/BoardController.java
@@ -1,16 +1,21 @@
 package controllers;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
 import models.Board;
 import views.BoardView;
+import views.PieceView;
 
 public class BoardController {
     private Board model;
     private BoardView view;
+    private PieceView[] buttons = new PieceView[42];
 
     public BoardController(Board b, BoardView bv) {
         this.model = b;
         this.view = bv;
-        view.setBoard(model); // connect view with model
     }
 
     public void setBoardDimension(int dimensionX, int dimensionY) {
@@ -20,5 +25,20 @@ public class BoardController {
 
     public void updateBoard() {
         view.repaint();
+    }
+
+    public void generateBoardNotations(JPanel p) {
+        for (int i = 0; i < 42; i++) {
+            PieceView button = new PieceView();
+            buttons[i] = button;
+            if (i % 2 == 0)
+                button.setBackground(Color.WHITE);
+            else
+                button.setBackground(Color.BLACK);
+            button.setOpaque(true);
+            button.setBorderPainted(false);
+            button.addActionListener(view);
+            p.add(button);
+        }
     }
 }
--- views/BoardView.java
+++ views/BoardView.java
@@ -9,59 +9,46 @@ import controllers.BoardController;
 
 @SuppressWarnings("serial")
 public class BoardView extends JFrame implements ActionListener {
-    private Board b = new Board();
-    private PieceView[] buttons = new PieceView[42];
-    // buttons = [
+    private Board model;
+    private BoardController controller;
+
+    // Board
     // 'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6',
     // 'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5',
     // 'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4',
     // 'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3',
     // 'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2',
-    // 'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1'
-    // ]
+    // 'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1',
 
-    public BoardView() {
+    public BoardView(Board model) {
         super("Talabia Chess");
         JPanel p = new JPanel();
         add(p);
 
-        for (int i = 0; i < 42; i++) {
-            PieceView button = new PieceView();
-            buttons[i] = button;
-            if (i % 2 == 0)
-                button.setBackground(Color.WHITE);
-            else
-                button.setBackground(Color.BLACK);
-            button.setOpaque(true);
-            button.setBorderPainted(false);
-            button.addActionListener(this);
-            p.add(button);
-        }
+        this.model = model;
+        controller = new BoardController(model, this);
+
+        // update Board to specified dimension
+        controller.updateBoard(); 
+        // generate board notations
+        controller.generateBoardNotations(p);
 
         p.setLayout(new GridLayout(6, 7));
-        setSize(b.getDimensionX(), b.getDimensionY());
+        setSize(model.getDimensionX(), model.getDimensionY());
         setVisible(true);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    public void setBoard(Board b) {
-        this.b = b;
     }
 
     public void repaint() {
         super.validate();
         super.repaint();
         super.pack();
-        super.setSize(b.getDimensionX(), b.getDimensionY());
+        super.setSize(model.getDimensionX(), model.getDimensionY());
     }
 
     public void actionPerformed(ActionEvent e) {
         PieceView btn = (PieceView) e.getSource();
         System.out.println("Button clicked! This button is " + btn.getCoordinate());
-    }
-
-    public PieceView[] getButtons() {
-        return buttons;
     }
 }
