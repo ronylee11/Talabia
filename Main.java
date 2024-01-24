6fc428854e3d4bce7902705dcbed491b4ea6a9f7
Rony Lee <rongyil33@gmail.com>
2024-01-24 16:22:28 +0800

    Remove redundant comments

--- Main.java
+++ Main.java
@@ -3,8 +3,6 @@ import models.Game;
 
 public class Main {
     public static void main(String[] args) {
-        // System.out.println("Talabia Chess");
-        // BoardView bv = new BoardView(new Board(500, 500));
         new Game();
     }
 }
