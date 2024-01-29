e21832eb13849b48dec99fe26c40d119265c3a33
Rony Lee <rongyil33@gmail.com>
2024-01-29 22:43:40 +0800

    Fix round and turn not updated after load game

--- views/BoardView.java
+++ views/BoardView.java
@@ -111,6 +111,7 @@ public class BoardView extends JFrame {
             } else {
                 JOptionPane.showMessageDialog(this, "No file name entered.");
             }
+            updateRoundAndPlayerLabels();
         });
         buttonsPanel.add(saveButton);
         buttonsPanel.add(loadButton);
